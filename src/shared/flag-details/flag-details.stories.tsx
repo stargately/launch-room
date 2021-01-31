8e4270f115ca3717bcf1afccdcd64ddcf507b07b
Tian Pan <puncsky@gmail.com>
2021-01-30 21:38:12 -0800

    refactor flag-details-controller

---
+++ src/shared/flag-details/flag-details.stories.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { themeDecorator } from "@/shared/common/storybook-utils";
+import { FlagDetails } from "@/shared/flag-details/flag-details";
+
+export const Standard: React.FC = () => (
+  <FlagDetails
+    flagDetails={{
+      on: true,
+      rules: [
+        {
+          id: "b47993ca-fb08-4eac-bc19-839fce50b1ac",
+          clauses: [
+            {
+              attribute: "email",
+              op: "endsWith",
+              values: ["@daommo.com"],
+              negate: false,
+            },
+          ],
+          variation: 0,
+          trackEvents: false,
+        },
+        {
+          id: "65947501-e647-44fc-b4e2-f4e025c07157",
+          clauses: [
+            {
+              attribute: "email",
+              op: "startsWith",
+              values: [
+                "puncsky",
+                "mengqiang.q",
+                "erickim987",
+                "ericjin987",
+                "therealchuhan",
+                "shiruisheng88@gmail.com",
+              ],
+              negate: false,
+            },
+          ],
+          variation: 0,
+          trackEvents: false,
+        },
+      ],
+      variations: [true, false],
+      fallthrough: { variation: 1 },
+    }}
+    flagKey="key"
+    upsertFlag={async () => undefined}
+    workspaceId="workspaceId"
+  />
+);
+
+export default {
+  title: "pages/flag-details/flag-details",
+  decorators: [themeDecorator()],
+  component: FlagDetails,
+};
---
+++ src/shared/flag-details/flag-details.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { ContentPadding } from "@/shared/common/styles/style-padding";
+import Switch from "antd/lib/switch";
+import { Rules } from "@/shared/flag-details/rules";
+import Form from "antd/lib/form";
+import notification from "antd/lib/notification";
+import Button from "antd/lib/button";
+import { Link } from "onefx/lib/react-router-dom";
+import { styled } from "onefx/lib/styletron-react";
+import { padding } from "polished";
+import Row from "antd/lib/row";
+// eslint-disable-next-line camelcase
+import { FlagDetails_flagDetails } from "@/shared/flag-details/data/__generated__/FlagDetails";
+import { UpsertFlagVariables } from "@/shared/flag-details/data/__generated__/UpsertFlag";
+import { RuleInput } from "../../../__generated__/globalTypes";
+import { CommonMargin } from "../common/common-margin";
+
+const StyledRow = styled(Row, ({ $theme }) => ({
+  ...padding($theme.sizing[2], 0),
+}));
+
+const StyledSpan = styled("span", ({ $theme }) => ({
+  ...padding(0, $theme.sizing[1]),
+}));
+
+type Props = {
+  flagKey: string;
+  // eslint-disable-next-line camelcase
+  flagDetails: FlagDetails_flagDetails | null | undefined;
+  upsertFlag: (variables: UpsertFlagVariables) => Promise<void>;
+  workspaceId: string;
+};
+
+export function FlagDetails({
+  flagDetails,
+  flagKey,
+  upsertFlag,
+  workspaceId,
+}: Props): JSX.Element {
+  return (
+    <ContentPadding>
+      <CommonMargin />
+      <StyledRow>
+        <Link to="../">Feature flags</Link>
+        <StyledSpan>/</StyledSpan>
+        {flagKey}
+      </StyledRow>
+      <h1>{flagKey}</h1>
+
+      <Form
+        onFinish={async (values) => {
+          const rules = [] as RuleInput[];
+          const valKeys = Object.keys(values);
+          for (const k of valKeys) {
+            if (k.startsWith("rules")) {
+              const [, i, attr, i2, attr2] = k.split(".");
+              const idx = parseInt(i, 10);
+              // @ts-ignore
+              rules[idx] = rules[idx] || {};
+              if (attr === "clauses") {
+                rules[idx].clauses = rules[idx].clauses || [];
+                // @ts-ignore
+                rules[idx].clauses[i2] = rules[idx].clauses[i2] || {};
+                // @ts-ignore
+                rules[idx].clauses[i2][attr2] = values[k];
+              } else {
+                // @ts-ignore
+                rules[idx][attr] = values[k];
+              }
+            }
+          }
+
+          await upsertFlag({
+            workspaceId,
+            key: flagKey,
+            on: values.on,
+            rules,
+          });
+
+          notification.success({ message: "Updated!" });
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Save Changes
+        </Button>
+        <Form.Item
+          name="on"
+          label="Is flag on?"
+          valuePropName="checked"
+          initialValue={flagDetails?.on}
+        >
+          <Switch />
+        </Form.Item>
+        <CommonMargin />
+        <Rules
+          rules={flagDetails?.rules || []}
+          variance={flagDetails?.variations || []}
+        />
+        If targeting is off, serve{" "}
+        {String(flagDetails?.variations[flagDetails?.fallthrough.variation])}
+      </Form>
+    </ContentPadding>
+  );
+}
