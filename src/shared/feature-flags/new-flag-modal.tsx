648cfdf93ccb301968bad2c144afb2db4e699e9f
chencheng <cheng@lajavaness.com>
2021-03-18 23:30:14 +0100

    feat:Add empty component when flags are empty

--- src/shared/feature-flags/new-flag-modal.tsx
+++ src/shared/feature-flags/new-flag-modal.tsx
@@ -1,20 +1,21 @@
 import Button from "antd/lib/button";
-import React, { useState } from "react";
 import Modal from "antd/lib/modal/Modal";
+import React, { useState } from "react";
 import { NewFlagForm } from "@/shared/feature-flags/new-flag-form";
 import { UpsertFlagVariables } from "../flag-details/data/__generated__/UpsertFlag";
 
 type Props = {
   action: (variables: UpsertFlagVariables) => Promise<void>;
+  newFlagLabel: string;
 };
 
-export const NewFlagModal: React.FC<Props> = ({ action }) => {
+export const NewFlagModal: React.FC<Props> = ({ action, newFlagLabel }) => {
   const [visible, setVisible] = useState(false);
   const close = () => setVisible(false);
   const open = () => setVisible(true);
   return (
     <>
-      <Button onClick={open}>+ Flag</Button>
+      <Button onClick={open}>{newFlagLabel}</Button>
       <Modal visible={visible} footer={null} onCancel={close}>
         <NewFlagForm upsertFlag={action} closeModal={close} />
       </Modal>
