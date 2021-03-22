bbaf2916d9d994e2e35ab0e34ed06154c70f8d96
chencheng <chenchengalex2014@gmail.com>
2021-03-22 09:07:04 +0100

    feat: Add and remove rule in flag details form (#70)
    Co-authored-by: chencheng <cheng@lajavaness.com>

--- src/shared/flag-details/flag-details.tsx
+++ src/shared/flag-details/flag-details.tsx
@@ -1,20 +1,23 @@
 import React from "react";
-import { ContentPadding } from "@/shared/common/styles/style-padding";
 import Switch from "antd/lib/switch";
-import { Rules } from "@/shared/flag-details/rules";
 import Form from "antd/lib/form";
-import notification from "antd/lib/notification";
 import Button from "antd/lib/button";
+import Row from "antd/lib/row";
+import notification from "antd/lib/notification";
+import Col from "antd/lib/col";
+import { t } from "onefx/lib/iso-i18n";
 import { Link } from "onefx/lib/react-router-dom";
 import { styled } from "onefx/lib/styletron-react";
 import { padding } from "polished";
-import Row from "antd/lib/row";
 // eslint-disable-next-line camelcase
 import { FlagDetails_flagDetails } from "@/shared/flag-details/data/__generated__/FlagDetails";
 import { UpsertFlagVariables } from "@/shared/flag-details/data/__generated__/UpsertFlag";
 import { VarianceSelect } from "@/shared/flag-details/variance-select";
+import { ContentPadding } from "@/shared/common/styles/style-padding";
+import { CommonMargin } from "@/shared/common/common-margin";
+import { Rules } from "@/shared/flag-details/rules";
+import deepOmit from "@/shared/utils/deep-omit";
 import { RuleInput } from "../../../__generated__/globalTypes";
-import { CommonMargin } from "../common/common-margin";
 
 const StyledRow = styled(Row, ({ $theme }) => ({
   ...padding($theme.sizing[2], 0),
@@ -38,6 +41,23 @@ export function FlagDetails({
   upsertFlag,
   workspaceId,
 }: Props): JSX.Element {
+  const [form] = Form.useForm();
+
+  const _onFinish = async (formData: Record<string, unknown>) => {
+    const values = deepOmit(formData, "__typename");
+
+    await upsertFlag({
+      workspaceId,
+      key: flagKey,
+      on: values.on as boolean,
+      rules: values.rules as RuleInput[],
+      fallthrough: values.fallthrough as { variation: number },
+      offVariation: values.offVariation as number,
+    });
+
+    notification.success({ message: t("notification.update") });
+  };
+
   return (
     <ContentPadding>
       <CommonMargin />
@@ -47,71 +67,54 @@ export function FlagDetails({
         {flagKey}
       </StyledRow>
       <h1>{flagKey}</h1>
-
       <Form
-        onFinish={async (values) => {
-          console.log(values);
-          const rules = [] as RuleInput[];
-          const valKeys = Object.keys(values);
-          for (const k of valKeys) {
-            if (k.startsWith("rules")) {
-              const [, i, attr, i2, attr2] = k.split(".");
-              const idx = parseInt(i, 10);
-              // @ts-ignore
-              rules[idx] = rules[idx] || {};
-              if (attr === "clauses") {
-                rules[idx].clauses = rules[idx].clauses || [];
-                // @ts-ignore
-                rules[idx].clauses[i2] = rules[idx].clauses[i2] || {};
-                // @ts-ignore
-                rules[idx].clauses[i2][attr2] = values[k];
-              } else {
-                // @ts-ignore
-                rules[idx][attr] = values[k];
-              }
-            }
-          }
-
-          await upsertFlag({
-            workspaceId,
-            key: flagKey,
-            on: values.on,
-            rules,
-            fallthrough: {
-              variation: values["fallthrough.variation"],
-            },
-            offVariation: values.offVariation,
-          });
-
-          notification.success({ message: "Updated!" });
-        }}
+        form={form}
+        initialValues={flagDetails || {}}
+        layout="vertical"
+        onFinish={_onFinish}
       >
         <Button type="primary" htmlType="submit">
           Save Changes
         </Button>
-        <Form.Item
-          name="on"
-          label="Is flag on?"
-          valuePropName="checked"
-          initialValue={flagDetails?.on}
-        >
-          <Switch />
-        </Form.Item>
+
         <CommonMargin />
-        <Rules
-          rules={flagDetails?.rules || []}
-          variance={flagDetails?.variations || []}
-          fallthroughVariance={flagDetails?.fallthrough.variation}
-        />
-        If targeting is off, serve{" "}
-        <VarianceSelect
-          itemProps={{
-            name: "offVariation",
-            initialValue: flagDetails?.offVariation,
-          }}
-          variance={flagDetails?.variations}
-          disabled={false}
-        />
+        <Row gutter={8} align="middle">
+          <Col flex="none">Is flag on? :</Col>
+          <Col flex="auto">
+            <Form.Item noStyle name="on" valuePropName="checked">
+              <Switch />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <CommonMargin />
+        <Form.Item
+          label="Target users who match these rules"
+          shouldUpdate={(prevValues, curValues) =>
+            prevValues.variations !== curValues.variations ||
+            prevValues.fallthrough !== curValues.fallthrough
+          }
+        >
+          {({ getFieldValue }) => (
+            <Rules variance={getFieldValue("variations")} />
+          )}
+        </Form.Item>
+        <Form.Item
+          shouldUpdate={(prevValues, curValues) =>
+            prevValues.variations !== curValues.variations
+          }
+        >
+          {({ getFieldValue }) => (
+            <VarianceSelect
+              itemProps={{
+                name: "offVariation",
+                label: "If targeting is off, serve",
+              }}
+              variance={getFieldValue("variations")}
+              disabled={false}
+            />
+          )}
+        </Form.Item>
       </Form>
     </ContentPadding>
   );
